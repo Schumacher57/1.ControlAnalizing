cf72ffb54daeb699bfb508a2b8c0b9810cdfef2b
Schumacher57 <ivanchenko.i91@mail.ru>
2021-03-11 16:12:09 +0600

    Little change for check control
    небольшие изменения для проверки контрола перед основными изменениями контрола

--- DataResource/ToggleButtonExample/ToggleButtonExample/Form1.cs
+++ DataResource/ToggleButtonExample/ToggleButtonExample/Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace ToggleButtonExample
@@ -15,6 +8,8 @@ namespace ToggleButtonExample
         public Form1()
         {
             InitializeComponent();
+            ceLearningToggle1.Click += (s, a) => { Debug.WriteLine($"Значение элемента toggleSwitch: {(s as CeLearningToggle).IsOn.ToString()}"); };
+
             //ceLearningToggle1.OnText = "  "+ "\u2714";
         }
     }
