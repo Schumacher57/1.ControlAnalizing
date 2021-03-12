885bc4fc00263376e9425f05ba78283db702991c
Schumacher57 <ivanchenko.i91@mail.ru>
2021-03-12 09:24:29 +0600

    Beginning try to change DataGridView.
    First steps to change DataGridView for adding host control (toggle switch).

---
+++ DataResource/AddHostControlOnDataGridView/NumberUpDownExample/DataGridViewNumUpDownCell.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberUpDownExample
+{
+    public class DataGridViewNumUpDownCell : DataGridViewTextBoxCell
+    {
+        public DataGridViewNumUpDownCell() : base()
+        { }
+
+        public override void InitializeEditingControl(int rowIndex, object initialFormattedValue,
+            DataGridViewCellStyle dataGridViewCellStyle)
+        {
+            base.InitializeEditingControl(rowIndex, initialFormattedValue, dataGridViewCellStyle);
+            CeLearningNumUpDown ctl = DataGridView.EditingControl as CeLearningNumUpDown;
+
+            if (this.Value == null)
+            {
+                ctl.Value = Convert.ToDouble(this.DefaultNewRowValue);
+            }
+            else
+            {
+                ctl.textbox.Text = this.Value.ToString();
+            }
+        }
+        public override Type EditType
+        {
+            get { return typeof(NumUpDownEditingControl); }
+        }
+
+        public override Type ValueType
+        {
+            get { return typeof(string); }
+        }
+
+        public override object DefaultNewRowValue
+        {
+            get { return 0; }
+        }
+    }
+}
---
+++ DataResource/AddHostControlOnDataGridView/NumberUpDownExample/DataGridViewNumUpDownColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberUpDownExample
+{
+    public class DataGridViewNumUpDownColumn : DataGridViewColumn
+    {
+        //DataGridViewNumUpDownCell this class i will generate later
+        public DataGridViewNumUpDownColumn() : base(new DataGridViewNumUpDownCell())
+        { }
+        public override DataGridViewCell CellTemplate
+        {
+            get { return base.CellTemplate; }
+            set
+            {
+                //Ensure that the cell used for the template is a NumericUpDown
+                if (value != null && !value.GetType().IsAssignableFrom(typeof(DataGridViewNumUpDownCell)))
+                {
+                    throw new InvalidCastException("Must be a string");
+                }
+                base.CellTemplate = value;
+            }
+        }
+    }
+}
